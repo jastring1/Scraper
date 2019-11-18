246cdd122c1b7bcb4bced4ca7748622495f893dd
jastring <tripp.stringfield@gmail.com>
2019-11-18 17:43:46 -0500

    initialized repository and node_modules

---
+++ server.js
@@ -0,0 +1,16 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
